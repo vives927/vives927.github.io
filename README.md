631fe990365305731dd507282c919b74d94cc92a
Genevieve Nelson <genevieven@c2mpg.com>
2014-08-19 16:45:43 -0400

    Adding highcharts Medicaid tables

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 vives927.github.io
 ==================
+
+Personal website for Genevieve Nelson, a playground of sorts.
---
+++ js/chart.js
@@ -0,0 +1,10 @@
+
+(function ($) {
+    'use strict';
+
+    // Highchart Table plugin
+    // https://github.com/highchartTable/jquery-highchartTable-plugin
+    $('table.highchart').highchartTable();
+
+
+}(jQuery));
