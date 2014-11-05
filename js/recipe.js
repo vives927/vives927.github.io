bab34e091823c891b7b0a5e8431c047a136e51f3
Genevieve Nelson <genevieven@c2mpg.com>
2014-11-05 17:00:42 -0500

    Adding recipes

--- js/recipe.js
+++ js/recipe.js
@@ -2,16 +2,27 @@
 (function ($) {
     'use strict';
 
-    // Sort recipes by category
     var $recipeList = $('#RecipeList');
-    $('#sortBy').change(function() {
-        var value = $(this).find('option:selected').attr('value');
+    
+    var sortList = function(value, attr) {
         if (value !== 'All') {
-            $recipeList.find('li').not('[data-category="' + value + '"]').addClass('Hidden');
-            $recipeList.find('li').filter('[data-category="' + value + '"]').removeClass('Hidden');
+            $recipeList.find('li').not('[data-' + attr + '="' + value + '"]').addClass('Hidden');
+            $recipeList.find('li').filter('[data-' + attr + '="' + value + '"]').removeClass('Hidden');
         } else {
             $recipeList.find('li').removeClass('Hidden');
         }
+    }
+
+    // Sort recipes by category
+    $('#sortByCategory').change(function() {
+        var value = $(this).find('option:selected').attr('value');
+        sortList(value, 'category');
+    });
+
+    // Sort recipes by source
+    $('#sortBySource').change(function() {
+        var value = $(this).find('option:selected').attr('value');
+        sortList(value, 'source');
     });
 
 }(jQuery));  
